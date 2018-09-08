3ec82a721183c4103279e423ff4a36339e703a78
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-09-08 13:49:02 +0700

    first commit

---
+++ SegmentObject.cpp
@@ -0,0 +1,13 @@
+//opencv
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
+//C++
+#include <iostream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
